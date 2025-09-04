ab2cd737a4b7c6d6a0de4fbfc994bebd3219f8d2
Eunice Mata <eunice.mata2992@gmail.com>
2025-09-04 17:14:45 -0600

    tool to read database

--- HistoricalLocalServer/historical_mcp.py
+++ HistoricalLocalServer/historical_mcp.py
@@ -1,4 +1,63 @@
 from mcp.server.fastmcp import FastMCP
+from fastmcp.exceptions import ToolError
+from typing import List, Optional, Dict, Any
+
+import json
+import sys
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv, dotenv_values
 
 # name for the MCP server
-mcp = FastMCP("HistoryClass")
+mcp = FastMCP(name="HistoryClass")
+
+load_dotenv()
+
+connectionString = os.getenv("CONNECTION_STRING")
+dbString = os.getenv("MONGODATABASE")
+collectionString = os.getenv("MONGOCOLLECTION")
+
+client = MongoClient(connectionString)
+db = client[dbString]
+collection = db[collectionString]
+
+@mcp.tool()
+def find_event(
+    title: Optional[str] = None,
+    tags: Optional[List[str]] = None,
+    date: Optional[str] = None
+) -> List[Dict[str,Any]]:
+    query = {}
+    if title:
+        query["title"] = {"$regex": title, "$options": "i"}
+    if tags:
+        query["tags"] = {"$in": tags}
+    if date:
+        query["date"] = date
+
+    try:
+        docs = list(collection.find(query))
+        for doc in docs:
+            doc["_id"] = str(doc["_id"])  # convertir ObjectId → str
+        return docs
+    except Exception as e:
+        raise ToolError(f"Error en find_event: {e}")
+    
+# Add a dynamic greeting resource
+@mcp.resource("greeting://{name}")
+def get_greeting(name: str) -> str:
+    """Get a personalized greeting"""
+    return f"Hello, {name}!"
+
+
+# Add a prompt
+@mcp.prompt()
+def greet_user(name: str, style: str = "friendly") -> str:
+    """Generate a greeting prompt"""
+    styles = {
+        "friendly": "Please write a warm, friendly greeting",
+        "formal": "Please write a formal, professional greeting",
+        "casual": "Please write a casual, relaxed greeting",
+    }
+
+    return f"{styles.get(style, styles['friendly'])} for someone named {name}."
